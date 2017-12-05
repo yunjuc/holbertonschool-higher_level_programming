46ccbb6ada5a49b7d1fbc5364d1ce4504d31c8d4
Yunju Chen <info@yunjuchen.net>
2017-12-05 23:22:46 +0000

    Fix pep8 style issues

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,10 @@ else:
     new = number
 last_digit = new % 10
 if 0 < last_digit < 6:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, last_digit))
 elif last_digit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit)) 
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit)) 
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, last_digit))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range (0, 9):
+for i in range(0, 9):
     for j in range(i + 1, 10):
         print("{:d}{:d}, ".format(i, j), end="")
-print("{:d}{:d}".format(i,j))
+print("{:d}{:d}".format(i, j))
