292bc541d0dfdb022da32049a29330ee8b50b83b
Yunju Chen <info@yunjuchen.net>
2017-12-06 19:57:23 +0000

    Remove extra last number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            break
         print("{:d}{:d}, ".format(i, j), end="")
 print("{:d}{:d}".format(i, j))
