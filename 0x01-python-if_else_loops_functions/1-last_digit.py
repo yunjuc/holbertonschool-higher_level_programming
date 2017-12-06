1fe0c3bbf9ccdc6ccde79a187f0bd067308504b2
Yunju Chen <info@yunjuchen.net>
2017-12-06 21:52:41 +0000

    Check if greater than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,9 @@ if number < 0:
     last_digit = abs(number) % 10 * -1
 else:
     last_digit = number % 10
-if 0 < last_digit < 6:
-    print("{:d} and is less than 6 and not 0".format(last_digit))
+if last_digit > 5:
+    print("{:d} and is greater than 5".format(last_digit))
 elif last_digit == 0:
     print("{:d} and is 0".format(last_digit))
-elif last_digit < 0:
-    print("{:d} and is less than 6 and not 0".format(last_digit))
 else:
-    print("{:d} and is greater than 5".format(last_digit))
+    print("{:d} and is less than 6 and not 0".format(last_digit))
