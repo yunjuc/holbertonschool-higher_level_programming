8d0af40ffb03621ef2a9241d343c2fc539f29211
Yunju Chen <info@yunjuchen.net>
2017-12-06 00:38:57 +0000

    First commit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+def print_last_digit(number):
+    '''print the last digit of a number'''
+    if number < 0:
+        number = -1 * number
+    number = number % 10
+    print(number, end="")
+    return number
