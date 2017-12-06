cc61ddb0331b47135e2933d11f1ce6ce231f2e68
Yunju Chen <info@yunjuchen.net>
2017-12-06 06:19:00 +0000

    First commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    '''print a string in upper case'''
+    for i in range(0, len(str)):
+        if 97 <= ord(str[i]) <= 122:
+            print(chr(ord(str[i]) - 32), end="")
+            continue
+        else:
+            print(str[i], end="")
+    print() 
