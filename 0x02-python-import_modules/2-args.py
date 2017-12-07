2ce87c11947eb178a68da35d89be95c39ee80588
Yunju Chen <info@yunjuchen.net>
2017-12-07 22:53:25 +0000

    First commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num = len(argv)
+    if num == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(num - 1))
+        for i in range(1, num):
+            print("{}: {}".format(i, argv[i]))
