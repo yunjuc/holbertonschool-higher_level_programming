289536fbbec442cde319661c7cfa71aac1e53435
Yunju Chen <info@yunjuchen.net>
2017-12-07 23:01:33 +0000

    Seperate 1 argument case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     num = len(argv)
     if num == 1:
         print("0 arguments.")
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
         print("{} arguments:".format(num - 1))
-        for i in range(1, num):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, num):
+        print("{}: {}".format(i, argv[i]))
