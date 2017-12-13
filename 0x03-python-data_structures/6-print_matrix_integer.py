9b135a2c6b2ff0340d4b532ac9039136333670d5
Yunju Chen <info@yunjuchen.net>
2017-12-13 21:34:07 +0000

    2nd commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print("{}".format(i))
+    if not matrix[0]:
+        return print()
+    for i in range(len(matrix)):
+        j = 0
+        for j in range(len(matrix)):
+            print("{}".format(matrix[i][j]), end="")
+            if j < len(matrix) - 1:
+                print(" ", end="")
+        print()
