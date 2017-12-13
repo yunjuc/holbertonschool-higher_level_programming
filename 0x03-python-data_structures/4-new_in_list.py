1566f781dbae9e457c89f94ebf987bbfb459b585
Yunju Chen <info@yunjuchen.net>
2017-12-13 19:44:59 +0000

    Change NONE to None, change len() to len()-1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return NONE
-    elif idx > len(my_list):
-        return NONE
+        return None
+    elif idx > len(my_list) - 1:
+        return None
     else:
         new_list = list.copy(my_list)
         new_list[idx] = element
