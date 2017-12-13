667f8de7cd9a8d6863e769bf009c89b0a83cfafa
Yunju Chen <info@yunjuchen.net>
2017-12-13 19:12:42 +0000

    Change NONE to None, change len() to len()-1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return NONE
-    elif idx > len(my_list):
-        return NONE
+        return None
+    elif idx > len(my_list) - 1:
+        return None
     else:
         my_list[idx] = element
         return my_list
