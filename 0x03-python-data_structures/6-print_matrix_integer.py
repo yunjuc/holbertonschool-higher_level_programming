9c1c1748b86053a0d09e00d7e6983d5958b0380f
Yunju Chen <info@yunjuchen.net>
2017-12-13 21:39:27 +0000

    Check edge case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         return print()
     for i in range(len(matrix)):
         j = 0
-        for j in range(len(matrix)):
+        for j in range(len(matrix[j])):
             print("{:d}".format(matrix[i][j]), end="")
-            if j < len(matrix) - 1:
+            if j < len(matrix[j]) - 1:
                 print(" ", end="")
         print()
