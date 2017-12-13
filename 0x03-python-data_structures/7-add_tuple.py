24adc5fe7c6b82522b3d25259e787a6e82b524c7
Yunju Chen <info@yunjuchen.net>
2017-12-13 23:55:35 +0000

    2nd commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    (a[0] + b[0], a[1] + b[1]) for a in tuple_a for b in touple_b
-        return (a, b)
+    x1, x2, y1, y2 = 0, 0, 0, 0
+    if len(tuple_a) == 0:
+        x1, x2 = 0, 0
+    elif len(tuple_a) == 1:
+        x1, x2 = tuple_a[0], 0
+    else:
+        x1, x2 = tuple_a[0], tuple_a[1]
+    if len(tuple_b) == 0:
+        y1, y2 = 0, 0
+    elif len(tuple_b) == 1:
+        y1, y2 = tuple_b[0], 0
+    else:
+        y1, y2 = tuple_b[0], tuple_b[1]
+    return (x1 + y1, x2 + y2)
