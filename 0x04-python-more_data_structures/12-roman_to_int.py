d76dc847f30edf2f0dd5c129852aa1d8b9ee840d
Yunju Chen <info@yunjuchen.net>
2017-12-15 21:06:55 +0000

    First commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,11 @@
 def roman_to_int(roman_string):
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
             'M': 1000} 
-    sum = 0
-    for i in roman_string:
-        if i in dict:
-            sum += dict[i]
-    return sum
+    total = 0
+    if roman_string:
+        for idx, c in enumerate(roman_string):
+            total += dict.get(c)
+            if idx <= len(roman_string) - 1:
+                if dict.get(roman_string[idx-1]) < dict.get(c):
+                    total -= 2 * dict.get(roman_string[idx-1])
+    return total
