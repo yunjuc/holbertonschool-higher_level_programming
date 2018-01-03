f2b5b64bf809b731d76f3581f211361fc9bac819
Yunju Chen <info@yunjuchen.net>
2018-01-03 06:31:29 +0000

    Trailing space

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
     for i in range(x):
-        try: 
+        try:
             print('{}'.format(my_list[i]), end="")
             num += 1
         except IndexError:
