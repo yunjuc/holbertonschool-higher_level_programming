76799ade4f889867415fa1faa9c3cf57ddc681b0
Yunju Chen <info@yunjuchen.net>
2018-01-03 06:30:08 +0000

    Refactoring

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num = 0
-    try:
-        for i in my_list:
-            if num < x:
-                print('{}'.format(i), end="")
-                num += 1
-        print()
-    except IndexError:
-        print('Error: index out of range')
+    for i in range(x):
+        try: 
+            print('{}'.format(my_list[i]), end="")
+            num += 1
+        except IndexError:
+            pass
+    print()
     return num
