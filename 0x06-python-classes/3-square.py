4ef13f91ff6a85c29c753e6902dfa0874011167a
Yunju Chen <info@yunjuchen.net>
2018-01-04 02:39:02 +0000

    First commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+class Square:
+    ''' Create a class Square.'''
+
+    def __init__(self, size=0):
+        '''Initialize Square class.
+
+        Args:
+            @size: Size of the square
+        '''
+        if not isinstance(size, int):
+            print("size must be an integer", end="")
+            raise TypeError
+        elif size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = size
+
+    def area(self):
+        '''Caculate the area of a squre
+
+        Returns:
+            Power of square size
+        '''
+        return self.__size**2
