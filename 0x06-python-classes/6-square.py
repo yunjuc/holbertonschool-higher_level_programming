61c027b713fce65058b141668872fb8ecb510205
Yunju Chen <info@yunjuchen.net>
2018-01-04 04:46:04 +0000

    First commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+
+class Square:
+    ''' Create a class Square.'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''Initialize Square class.
+
+        Args:
+        @size: Size of the square.
+        @poistion: Starting point of the square.
+        '''
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        '''Set size as property.
+
+        Returns:
+        Value of size.
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Setter of size.
+
+        Args:
+        @value: value of size.
+        '''
+        if not isinstance(value, int):
+            print("size must be an integer", end="")
+            raise TypeError
+        elif value < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        '''Set position as property.
+
+        Returns:
+        Coordination of position.
+        '''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        '''Setter of position coordination.
+
+        Args:
+        @value: coordination of position.
+        '''
+        if value[0] < 0 or isinstance(value[0], int) is False or \
+           value[1] < 0 or isinstance(value[1], int) is False or \
+           len(value) != 2:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
+        else:
+            self.__position = value
+
+    def area(self):
+        '''Caculate the area of a squre.
+
+        Returns:
+        Power of square size.
+        '''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''Print a square with character #.'''
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                for col in range(self.__size):
+                    print("#", end="")
+                print()
