e1deefb6313de746d7a45ffc271b8a82c300af26
Yunju Chen <info@yunjuchen.net>
2018-01-04 02:27:28 +0000

    First commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    ''' Create a class Square.'''
+
+    def __init__(self, size):
+        '''Initialize Square class.
+
+        Args:
+            @size: Size of the square
+        '''
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class Square:
+    ''' Create a class Square.'''
+
+    def __init__(self, size=0):
+        '''Initialize Square class.
+
+        Args:
+            @size: Size of the square
+        '''
+        if not isinstance(size, int):
+            print("size must be an integer", end="")
+            raise TypeError
+        elif size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = size
