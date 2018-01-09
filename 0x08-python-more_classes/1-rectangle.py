e84a85bc8a1dfe5028fe9214083e11d6ba91e8cc
Yunju Chen <info@yunjuchen.net>
2018-01-09 11:48:41 +0000

    Typo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -34,5 +34,5 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
