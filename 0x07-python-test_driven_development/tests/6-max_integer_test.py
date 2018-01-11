36d5893573e0909dd8475f105292b272871cf546
Yunju Chen <info@yunjuchen.net>
2018-01-11 06:38:00 +0000

    Add test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,6 +12,21 @@ class TestMaxInteger(unittest.TestCase):
         result = max_integer([1, 3, 3, 2])
         self.assertEqual(result, 3)
 
+    def max_first(self):
+        '''Test a normal list'''
+        result = max_integer([8, 3, 3, 2])
+        self.assertEqual(result, 8)
+
+    def max_end(self):
+        '''Test a normal list'''
+        result = max_integer([1, 3, 3, 10])
+        self.assertEqual(result, 10)
+
+    def one_element(self):
+        '''Test a normal list'''
+        result = max_integer([5])
+        self.assertEqual(result, 5)
+
     def test_empty_list(self):
         '''Empty list'''
         result = max_integer([])
@@ -26,6 +41,12 @@ class TestMaxInteger(unittest.TestCase):
         '''Test if list has a string'''
         with self.assertRaises(TypeError):
             max_integer([1, 'hello', 2, 3])
+
+    def test_float(self):
+        '''Test if list has float'''
+        with self.assertRaises(TypeError):
+           max_integer([1, 2, -3, 5.5])
+
 
 if __name__ == "__main__":
     unittest.main()
