f9df5a0706fccb940dbb44c07b7dd84fed9e10f1
Yunju Chen <info@yunjuchen.net>
2018-01-18 19:42:31 +0000

    Use try/except to check if file exists

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,13 +4,16 @@ from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = []
 filename = "add_item.json"
 
-if __name__ == "__main__":
-    my_list = load_from_json_file(filename)
-    i = 1
-    while i < len(argv):
-        my_list.append(argv[i])
-        i += 1
-    save_to_json_file(my_list, filename)
+try:
+    with open(filename, 'r+') as my_list:
+        my_list = load_from_json_file(filename)
+except:
+    my_list = []
+
+i = 1
+while i < len(argv):
+    my_list.append(argv[i])
+    i += 1
+save_to_json_file(my_list, filename)
