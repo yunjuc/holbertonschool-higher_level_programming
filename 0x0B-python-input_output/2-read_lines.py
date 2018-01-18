d5237ddfdc55a165ea74198c72141125be1f42af
Yunju Chen <info@yunjuchen.net>
2018-01-18 20:55:28 +0000

    Reset f position after looping

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,10 +7,12 @@ def read_lines(filename="", nb_lines=0):
         count = 0
         for line in f:
             count += 1
+        f.seek(0)
         if nb_lines <= 0 or nb_lines >= count:
             print(f.read(), end="")
         else:
             i = 0
-            while i < nb_lines:
-                print(f.readline())
+            for line in f:
+                if i < nb_lines:
+                    print(line, end="")
                 i += 1
