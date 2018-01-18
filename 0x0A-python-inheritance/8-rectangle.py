e4bfb0381d35bb1bb7a3659d978392996eef64fc
Yunju Chen <info@yunjuchen.net>
2018-01-18 07:03:02 +0000

    Remove set default value in __init__

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-
-
-class BaseGeometry:
-    '''an base Geometry class'''
-
-    def area(self):
-        '''raise an exception'''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        '''validate name and value'''
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     '''Rectangle class from BaseGeometry'''
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, width, height):
         '''Rectangle instantiation'''
-        self.__width = width
+        self.__widht = width
         self.__height = height
         Rectangle.integer_validator(self, "width", width)
         Rectangle.integer_validator(self, "height", height)
