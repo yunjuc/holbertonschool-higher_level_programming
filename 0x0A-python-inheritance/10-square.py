127b2005fde4e0660275ab3085bd0fdb3ad9cfaf
Yunju Chen <info@yunjuchen.net>
2018-01-18 07:24:37 +0000

    Add Square value validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,38 +1,5 @@
 #!/usr/bin/python3
-
-
-class BaseGeometry:
-    '''an base Geometry class'''
-
-    def area(self):
-        '''raise an exception'''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        '''validate name and value'''
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    '''Rectangle class from BaseGeometry'''
-
-    def __init__(self, width=0, height=0):
-        '''Rectangle instantiation'''
-        self.__width = width
-        self.__height = height
-        Rectangle.integer_validator(self, "width", width)
-        Rectangle.integer_validator(self, "height", height)
-
-    def __str__(self):
-        '''Rectangle definition'''
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        '''define rectangle area'''
-        return self.__width * self.__height
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -41,6 +8,7 @@ class Square(Rectangle):
     def __init__(self, size):
         '''Square instantiation'''
         self.__size = size
+        Square.integer_validator(self, "size", size)
         super().__init__(size, size)
 
     def area(self):
