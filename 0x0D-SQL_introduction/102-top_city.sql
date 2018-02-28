bf8cd5fea0345a935475ecfc43b12de4e5079709
Yunju Chen <info@yunjuchen.net>
2018-02-28 20:20:36 +0000

    Change top to limit

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- displays the average temperature by city ordered by temperature
-SELECT TOP 3 city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
