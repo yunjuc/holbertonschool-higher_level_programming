2d6ce1b4702ee7e0b279841d587fef17fa2dc1f3
Yunju Chen <info@yunjuchen.net>
2018-03-01 07:48:44 +0000

    order by asc

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- list all shows contained in the database without a genre linked
-SELECT title, genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE genre_id IS NULL ORDER BY title, genre_id;
+SELECT title, genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE genre_id IS NULL ORDER BY title, genre_id ASC;
