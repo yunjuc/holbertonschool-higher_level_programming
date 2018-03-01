0c132da20ad93d483d5b678d9bf92ce5f8eae721
Yunju Chen <info@yunjuchen.net>
2018-03-01 21:33:28 +0000

    Change to subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- find all California cities in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id=1 ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id =
+(SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
