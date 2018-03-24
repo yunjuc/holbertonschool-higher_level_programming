e90297000eef50587729848fc5fe8e6b6ced125c
Yunju Chen <info@yunjuchen.net>
2018-03-24 07:43:04 +0000

    Add case sensitive check

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY\
-                states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'N%'\
+                ORDER BY states.id ASC")
     state = cur.fetchall()
     for row in state:
         print(row)
