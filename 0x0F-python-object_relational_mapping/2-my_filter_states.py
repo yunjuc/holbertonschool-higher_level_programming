aa63f9f32deab19ca1f11b054087fa06f983c0fd
Yunju Chen <info@yunjuchen.net>
2018-03-24 07:53:32 +0000

    Check case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"\
+                .format(sys.argv[4]))
     state = cur.fetchall()
     for row in state:
         print(row)
