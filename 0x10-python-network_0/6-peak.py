19f71ad2bf5ac03a209011e412faa45a06df7d59
Yunju Chen <info@yunjuchen.net>
2018-04-03 23:17:58 +0000

    First commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Finds a peak in a list of unsorted integers'''
+
+
+def find_peak(list_of_integers):
+    '''Find the peak'''
+    for i in range(1, len(list_of_integers)):
+        if list_of_integers[i] < list_of_integers[i-1]:
+            return (list_of_integers[i-1])
