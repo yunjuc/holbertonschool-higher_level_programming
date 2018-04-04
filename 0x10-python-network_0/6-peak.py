7fddaf79e2055084e98812750d0ee27e207d610f
Yunju Chen <info@yunjuchen.net>
2018-04-04 01:00:03 +0000

    Return 1st element when no peak found

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,3 +7,4 @@ def find_peak(list_of_integers):
     for i in range(1, len(list_of_integers)):
         if list_of_integers[i] < list_of_integers[i-1]:
             return (list_of_integers[i-1])
+    return list_of_integers[0]
