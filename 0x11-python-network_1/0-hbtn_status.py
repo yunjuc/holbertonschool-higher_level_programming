2d5616f9ff29d2943c1d53086d4962d9eac3338c
Yunju Chen <info@yunjuchen.net>
2018-04-09 22:35:28 +0000

    Use \t

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
         body = r.read()
         print('Body response:')
-        print('    - type: {}'.format(type(body)))
-        print('    - content: {}'.format(body))
-        print('    - utf8 content: {}'.format(body.decode()))
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode()))
