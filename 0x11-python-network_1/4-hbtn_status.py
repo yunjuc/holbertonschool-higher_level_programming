84d684207b07546330e85f3381de5975acdd9a75
Yunju Chen <info@yunjuchen.net>
2018-04-09 22:36:22 +0000

    Use \t

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     r = requests.get(url)
     print('Body response:')
-    print('    -type: {}'.format(type(r.text)))
-    print('    -content: {}'.format(r.text))
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
