8ce84965561cbfcab8f9fb247005d09406866e8a
Yunju Chen <info@yunjuchen.net>
2018-04-10 01:29:39 +0000

    Check argv length

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,8 +6,8 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    if sys.argv[1] is None:
-        q=""
+    if len(sys.argv) == 1:
+        q = ""
     else:
         q = sys.argv[1]
     value = {'q': q}
