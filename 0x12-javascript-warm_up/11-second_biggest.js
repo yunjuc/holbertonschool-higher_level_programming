1a26c3f2b579c5c26f46824dd76257fd9c279b50
Yunju Chen <info@yunjuchen.net>
2018-05-02 01:06:48 +0000

    Fix style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let arg = []
+let arg = [];
 if (process.argv.length < 4) {
   console.log(0);
 } else {
   for (let i = 2; i < process.argv.length; i++) {
-    arg.push(process.argv[i])
-  } 
+    arg.push(process.argv[i]);
+  }
   arg.sort();
   console.log(arg[arg.length - 2]);
 }
