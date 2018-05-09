a0d111c38a39c3b6b539562a3fd39745dbdf67c4
Yunju Chen <info@yunjuchen.net>
2018-05-09 17:40:29 +0000

    Add https

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,14 @@
 
 const request = require('request');
 const url = process.argv[2];
-const id = 'https://swapi.co/api/people/18/';
+const id1 = 'https://swapi.co/api/people/18/';
+const id2 = 'http://swapi.co/api/people/18/';
 request(url, function (error, response, body) {
   if (error) console.log('error:', error);
   const data = JSON.parse(body);
   let count = 0;
   for (let i in data.results) {
-    if (data.results[i].characters.includes(id)) count++;
+    if (data.results[i].characters.includes(id1)||data.results[i].characters.includes(id2)) count++;
   }
   console.log(count);
 });
