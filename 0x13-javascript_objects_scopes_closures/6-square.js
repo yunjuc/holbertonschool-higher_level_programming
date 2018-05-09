04d6c922156a18635d0568a3e30bd260879ec12b
Yunju Chen <info@yunjuchen.net>
2018-05-09 11:02:12 -0700

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle.js');
+const Square = require('./5-square.js');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (!c) {
       super.print();
