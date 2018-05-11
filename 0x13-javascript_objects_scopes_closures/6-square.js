7b0b1246a98bf65043de2c3d684816a8fe14e8ef
Yunju Chen <info@yunjuchen.net>
2018-05-11 03:55:56 +0000

    Change size to width/height

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const Square = require('./5-square.js');
+const baseSquare = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends baseSquare {
   charPrint (c) {
     if (!c) {
-      super.print();
+      this.print();
     } else {
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
           process.stdout.write(c);
         }
         process.stdout.write('\n');
